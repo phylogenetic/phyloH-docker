03897d227008f6d0a9081fefbdd80d98007f597c
maricaantonacci <marica.antonacci@gmail.com>
2016-06-10 07:40:01 +0000

    Log and exit code management modified.\nThe log file is always generated and uploaded to Swift (unless Swift is not working)

--- run.sh
+++ run.sh
@@ -2,21 +2,30 @@
 
 # Branch of the phyloH repo - default to master
 GITBRANCH=${GITBRANCH:-master}
+LOG_FILE=${LOG_FILE:-run.log}
+OUTPUT_PREFIX=${OUTPUT_PREFIX:-out}
 
-cd $WORKDIR
+cd "$WORKDIR"
+
+# Redirect stderr and stdout to the log file
+exec 1<&-
+exec 2<&-
+
+exec 1<>"$LOG_FILE"
+exec 2>&1
 
 # clone the code repo
-git clone https://github.com/svicario/phyloH.git -b $GITBRANCH || exit 1
+git clone https://github.com/svicario/phyloH.git -b "$GITBRANCH"
 
 # run the job execution
-python phyloH/esecutorePhyloHPandas.py -f $PHYLOGENY_FILENAME  -s $SAMPLE_FILENAME -g $GROUP_FILENAME -q 1 -r 2 -o $OUTPUT -e 0 -k 0 || exit 1
+python phyloH/esecutorePhyloHPandas.py -f "$PHYLOGENY_FILENAME" -s "$SAMPLE_FILENAME" -g "$GROUP_FILENAME" -q 1 -r 2 -o "$OUTPUT_PREFIX" -e 0 -k 0 
 
 # collect the output
-tar cvfz $OUTPUT.tgz external/ $OUTPUT* radial2.js || exit 1
+tar cvfz "$OUTPUT".tgz --ignore-failed-read external/ "$OUTPUT_PREFIX"* radial2.js "$LOG_FILE" 
 
 # upload the output to the Object Storage
-swift --insecure upload $OUTPUT $OUTPUT.tgz 
+swift --insecure upload "$OUTPUT" "$OUTPUT".tgz 
 
 # set read ACL on the container
-swift --insecure post -r '.r:*' $OUTPUT
+swift --insecure post -r '.r:*' "$OUTPUT"
 
