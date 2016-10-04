8eef305064ca58aca53124890682562c91356062
svicario <saverio.vicario@gmail.com>
2016-10-04 15:58:46 +0200

    EnvVar for full functionality
    Adding EnvVar to match full functionality of phyloH. Some EnvVar would match both flag and value so that when option is not required the entire flag could be skiped.

--- run.sh
+++ run.sh
@@ -4,6 +4,24 @@
 GITBRANCH=${GITBRANCH:-master}
 LOG_FILE=${LOG_FILE:-run.log}
 OUTPUT_PREFIX=${OUTPUT_PREFIX:-out}
+GEOADD=${GEOADD:-}
+TAXONOMY_FILENAME=${TAXONOMY_FILENAME:-}
+PHYLOGENY_FILENAME=${PHYLOGENY_FILENAME:-}
+EQUALQUANTITY=${$EQUALQUANTITY:-0}
+PAIRSWISE=${$PAIRSWISE:-0}
+if ["$GEOADD"!=""] then 
+$GEOADD="-G 1"
+fi
+if ["$TAXONOMY_FILENAME"!=""] then 
+$TAXONOMY_FILENAME="-t $TAXONOMY_FILENAME"
+fi
+if ["$PHYLOGENY_FILENAME"!=""] then 
+$PHYLOGENY_FILENAME="-f $PHYLOGENY_FILENAME"
+fi
+
+if ["$GROUP_FILENAME"!=""] then
+$GROUP_FILENAME="-g $GROUP_FILENAME"
+fi
 
 cd "$WORKDIR"
 
@@ -18,7 +36,7 @@ exec 2>&1
 git clone https://github.com/svicario/phyloH.git -b "$GITBRANCH"
 
 # run the job execution
-python phyloH/esecutorePhyloHPandas.py -f "$PHYLOGENY_FILENAME" -s "$SAMPLE_FILENAME" -g "$GROUP_FILENAME" -q 1 -r "$Nrandomization" -o "$OUTPUT_PREFIX" -e 0 -k 0 
+python phyloH/esecutorePhyloHPandas.py "$GEOADD" "$PHYLOGENY_FILENAME" -s "$SAMPLE_FILENAME" "$GROUP_FILENAME" -r "$Nrandomization" -o "$OUTPUT_PREFIX" -e "$EQUALQUANTITY" -k "$PAIRSWISE" "$TAXONOMY_FILENAME"
 
 # collect the output
 tar cvfz "$OUTPUT".tgz --ignore-failed-read external/ "$OUTPUT_PREFIX"* radial2.js "$LOG_FILE" 
